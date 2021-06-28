4d4359ccb0b65665010f1b903b80029463218f42
Rohit Parab <rohitparab213@gmail.com>
2021-06-28 18:36:25 +0530

    Created Product component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './Components/Header';
 import './App.css';
 import React from "react"
+import Home from './Components/Home'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* Header */}
       <Header/>
       {/* Home */}
+      <Home/>
     </div>
   );
 }
