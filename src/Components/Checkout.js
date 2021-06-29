e7b22bbbec95ba024fb39d5c8f047c1097f93383
Rohit Parab <rohitparab213@gmail.com>
2021-06-29 14:38:44 +0530

    Created subtotal component and checkout page

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Checkout.css'
+import Subtotal from './Subtotal'
 
 function Checkout() {
     return (
         <div className="checkout">
-            <h1>Checkout Page</h1>
+            <div className="checkout__left">
+                <img className="checkout__ad" src="https://wordstream-files-prod.s3.amazonaws.com/s3fs-public/styles/simple_image/public/images/media/images/banner-ads-examples-apple.jpg?uklBv0VgNUUcdJW.n6XoIOUC0K8BDW5e&itok=6QDTO-Fg" alt=""/>
+            
+
+                <div>
+                    <h2 className="checkout__title">
+                        Your shopping basket
+                    </h2>
+                    {/* BasketItems */}
+                </div>
+
+            </div>
+
+            <div className="checkout__right">
+                <Subtotal/>
+            </div>
         </div>
     )
 }
