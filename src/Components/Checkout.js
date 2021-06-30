e99f15fb5458a7caf0dcc57407badda0d774e067
Rohit Parab <rohitparab213@gmail.com>
2021-06-30 19:16:46 +0530

    Created CheckoutProduct.js

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Checkout.css'
 import Subtotal from './Subtotal'
 import { useStateValue } from '../StateProvider';
+import CheckoutProduct from './CheckoutProduct'
 
 
 function Checkout() {
@@ -19,7 +20,11 @@ function Checkout() {
                         Your shopping basket
                     </h2>
 
-                
+                    {basket?.map((bucket)=><CheckoutProduct />)}
+                    {/* CheckoutProduct */}
+                    {/* CheckoutProduct */}
+                    {/* CheckoutProduct */}
+                    {/* CheckoutProduct */}
 
                 </div>
 
