f227a709a5be18a65a233b766775f0c7f5d5152b
Rohit Parab <rohitparab213@gmail.com>
2021-07-01 19:27:46 +0530

    Setup handles for sigin and register

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import {Link} from 'react-router-dom'
 
 function Login() {
+    const [email,setEmail] = useState();
+    const [password,setPassword] = useState();
+
+    const signIn = (e) =>{
+        e.preventDefault();
+    }
+
+    const register = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className="login">
             <Link to="/">
@@ -12,13 +23,21 @@ function Login() {
                 <h1>Sign-In</h1>
                 <form className="login__form">
                     <label><strong>Email or mobile phone number</strong></label>
-                    <input type="text"/>
+                    <input type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
                     <label><strong>Password</strong></label>
-                    <input type="password"/>
-                    <button className="login__SignInButton">Sign In</button>
+                    <input type="password"
+                        value={password}
+                        onChange={e=>setPassword(e.target.value)}
+                    />
+                    <button type="submit" className="login__signInButton"
+                        onClick={signIn}
+                    >Sign In</button>
                 </form>
                 <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
-                <button className="login__registerButton">Create an account</button>
+                <button onClick={register} className="login__registerButton">Create an account</button>
             </div>
         </div>
     )
